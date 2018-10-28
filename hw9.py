8e23d2bf46e13ff98e555b32d77f31c1eb2f323b
AnastasiaVorton <anastasia.vorton@gmail.com>
2018-10-28 14:17:43 +0300

    Finish algorithm implementation.

--- hw9.py
+++ hw9.py
@@ -1,17 +1,16 @@
 import numpy as np
+import scipy.stats as sp
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt2
 
 
 def accuracy(predictions, y_test):
-    """
-    Evaluates the accuracy of the prediction
-    """
+    """ Evaluates the accuracy of the prediction """
     correct = 0
     for t in zip(predictions, y_test):
         if t[0] == t[1]:
@@ -19,9 +18,27 @@ def accuracy(predictions, y_test):
     return (correct / float(len(y_test))) * 100.0
 
 
-"""
-Preparing dataset
-"""
+def my_boost(iboost, X, y, sample_weight, n_est, estimator, estimator_weight, estimator_error):
+    """ Implements sample weights update based on estimators prediction,
+     weight and error on every iteration of Adaboost """
+    estimator.fit(X, y, sample_weight=sample_weight)
+    y_predict = estimator.predict(X)
+    # Instances incorrectly classified
+    incorrect = y_predict != y
+    # Stop if classification is perfect
+    if estimator_error <= 0:
+        return sample_weight, 1., 0.
+    # Only boost the weights if I will fit again
+    if not iboost == n_est - 1:
+        for i in range(len(sample_weight)):
+            if incorrect[i]:
+                sample_weight[i] *= np.exp(estimator_weight)
+            else:
+                sample_weight[i] *= np.exp(-1 * estimator_weight)
+    return sample_weight / sum(sample_weight)
+
+
+""" Preparing dataset"""
 # reading the dataset
 countries = pd.read_csv('/Users/anastasia/Documents/IU/Fall18/Machine learning/Labs/datasets/Countries.csv')
 # resetting region values to be 0 or 1 (EUROPE - 0, otherwise - 1)
@@ -41,29 +58,39 @@ countries_dataset_cleaned = countries.reset_index(drop=True)
 X_1 = countries_dataset_cleaned.drop('Region', axis=1)
 y_1 = countries_dataset_cleaned['Region']
 
-
-"""
-Training adaboost classifier to find outliers 
-"""
+""" Training adaboost classifier to find outliers """
+# model preparation
 d_tree = tree.DecisionTreeClassifier(max_leaf_nodes=4, max_features=5)
-n_est = 22
-model_for_outliers = AdaBoostClassifier(base_estimator=d_tree, n_estimators=n_est)
-model_for_outliers, weights = model_for_outliers.fit(X_1, y_1)
+n_est = 50
+model_for_outliers = AdaBoostClassifier(base_estimator=d_tree, n_estimators=n_est, algorithm='SAMME')
+model_for_outliers = model_for_outliers.fit(X_1, y_1)
 
+# process of finding outliers
+sample_weight = np.empty(X_1.shape[0], dtype=np.float64)
+sample_weight[:] = 1. / X_1.shape[0]
+estimators = model_for_outliers.estimators_
+estimator_weights = model_for_outliers.estimator_weights_
+estimator_errors = model_for_outliers.estimator_errors_
 
-"""
-Finding outliers
-"""
+# iterating to update sample weights
+for iboost in range(n_est):
+    sample_weight = my_boost(iboost, X_1, y_1, sample_weight, n_est, estimators[iboost],
+                             estimator_weights[iboost], estimator_errors[iboost])
+weights = sample_weight
+
+""" Finding outliers """
 # finding outliers
-average = sum(weights)/len(weights)
 outliers_ind = []
-for i in range(len(weights)):
-    if weights[i] > average:
+# using standard score and a tuned constant that in my opinion performs well in our situation
+scores = sp.zscore(weights)
+for i in range(len(scores)):
+    if scores[i] > 1.5:
         outliers_ind.append(countries['Country'][i])
 
 # outliers plot
 indices = [i for i in range(len(weights))]
 plt.scatter(indices, weights, color='red')
+plt.show()
 
 # droping outliers
 countries_cleaned = countries_dataset_cleaned.copy()
@@ -71,35 +98,35 @@ for i in outliers_ind:
     countries_cleaned = countries_cleaned[countries_cleaned['Country'] != i]
 countries_cleaned = countries_cleaned.reset_index(drop=True)
 
-"""
-Training the model with outliers 
-"""
+""" Training the model with outliers """
 # creating test and train sets
-X_train, X_test, y_train, y_test = train_test_split(X_1, y_1)
-model_with_outliers = AdaBoostClassifier(base_estimator=d_tree, n_estimators=n_est)
-model_with_outliers, weights = model_with_outliers.fit(X_train, y_train)
-predictions = model_with_outliers.predict(X_test)
-print("Before: ", accuracy(predictions, y_test))
+accuracies = []
+for i in range(10):
+    X_train, X_test, y_train, y_test = train_test_split(X_1, y_1)
+    n_est = 22
+    model_with_outliers = AdaBoostClassifier(base_estimator=d_tree, n_estimators=n_est)
+    model_with_outliers = model_with_outliers.fit(X_train, y_train)
+    predictions = model_with_outliers.predict(X_test)
+    acc = accuracy(predictions, y_test)
+    accuracies.append(acc)
+ind = [i for i in range(1, len(accuracies)+1)]
+plt2.plot(ind, accuracies, color='blue', label='with outliers')
 
-"""
-Training the model without outliers
-"""
+
+""" Training the model without outliers """
 X_2 = countries_cleaned.drop('Region', axis=1)
 y_2 = countries_cleaned['Region']
-X_train, X_test, y_train, y_test = train_test_split(X_2, y_2)
+accuracies_after = []
+for i in range(10):
+    X_train, X_test, y_train, y_test = train_test_split(X_2, y_2)
 
-model_without_outliers = AdaBoostClassifier(base_estimator=d_tree, n_estimators=n_est)
-model_without_outliers, weights_2 = model_without_outliers.fit(X_train, y_train)
-predictions = model_without_outliers.predict(X_test)
-print("After: ", accuracy(predictions, y_test))
-
-indices_2 = [i for i in range(len(weights_2))]
-plt.scatter(indices_2, weights_2, color='blue')
-plt.show()
-
-
-
-
-
-
+    model_without_outliers = AdaBoostClassifier(base_estimator=d_tree, n_estimators=n_est)
+    model_without_outliers = model_without_outliers.fit(X_train, y_train)
+    predictions = model_without_outliers.predict(X_test)
+    acc_2 = accuracy(predictions, y_test)
+    accuracies_after.append(acc_2)
+plt2.plot(ind, accuracies_after, color='green', label='without outliers')
+plt2.title("Accuracies compared")
+plt2.legend()
+plt2.show()
 
